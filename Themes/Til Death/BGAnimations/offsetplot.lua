b3a1d3d153d56e98175fdeef9e72d2c16da89486
xwidghet <xwidghet@yahoo.com>
2017-03-22 04:28:05 -0700

    Use one MultiVertex quad actor for the judgment plot table instead of one quad actor for each judgment.

--- Themes/Til Death/BGAnimations/offsetplot.lua
+++ Themes/Til Death/BGAnimations/offsetplot.lua
@@ -3,7 +3,7 @@ local tst = { 1.50,1.33,1.16,1.00,0.84,0.66,0.50,0.33,0.20 }
 
 local plotWidth, plotHeight = 400,120
 local plotX, plotY = SCREEN_WIDTH - 9 - plotWidth/2, SCREEN_HEIGHT - 56 - plotHeight/2
-local dotDims, plotMargin = 2, 4
+local dotDims, plotMargin = 1, 4
 local maxOffset = 180*tst[judge]
 local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1)
 local dvt = pss:GetOffsetVector()
@@ -17,24 +17,6 @@ end
 
 local function fitY(y)	-- Scale offset values to fit within plot height
 	return -1*y/maxOffset*plotHeight/2
-end
-
-local function plotOffset(nr,dv)
-	if dv == 1000 then 	-- 1000 denotes a miss for which we use a different marker
-		return Def.Quad{InitCommand=cmd(xy,fitX(nr),fitY(tst[judge]*184);zoomto,dotDims,dotDims;diffuse,offsetToJudgeColor(dv/1000);valign,0)}
-	end
-	return Def.Quad{
-		InitCommand=cmd(xy,fitX(nr),fitY(dv);zoomto,dotDims,dotDims;diffuse,offsetToJudgeColor(dv/1000)),
-		JudgeDisplayChangedMessageCommand=function(self)
-			local pos = fitY(dv)
-			if math.abs(pos) > plotHeight/2 then
-				self:y(fitY(tst[judge]*184))
-			else
-				self:y(pos)
-			end
-			self:diffuse(offsetToJudgeColor(dv/1000, tst[judge]))
-		end
-	}
 end
 
 local o = Def.ActorFrame{
@@ -64,10 +46,41 @@ local wuab = {}
 for i=1,#nrt do
 	wuab[i] = td:GetElapsedTimeFromNoteRow(nrt[i])
 end
+
 -- Plot Dots
-for i=1,#devianceTable do
-	o[#o+1] = plotOffset(wuab[i], dvt[i])
-end
+local verts= {}
+o[#o+1] = Def.ActorMultiVertex{
+		Name= "AMV_Quads",
+		InitCommand=function(self)
+			self:visible(true)
+			self:xy(0, 0)
+			for i=1,#devianceTable do
+				local color = offsetToJudgeColor(dvt[i]/1000);
+				if dvt[i] == 1000 then -- 1000 denotes a miss for which we use a different marker
+					local x = fitX(wuab[i]);
+					local y = fitY(tst[judge]*183);
+					verts[#verts+1] = {{x-dotDims,y+dotDims,0}, color}
+					verts[#verts+1] = {{x+dotDims,y+dotDims,0}, color}
+					verts[#verts+1] = {{x+dotDims,y-dotDims,0}, color}
+					verts[#verts+1] = {{x-dotDims,y-dotDims,0}, color}
+				else
+					local x = fitX(wuab[i]);
+					local y = fitY(dvt[i]);
+					if math.abs(y) > plotHeight/2 then
+						y = fitY(tst[judge]*183);
+					end
+					verts[#verts+1] = {{x-dotDims,y+dotDims,0}, color}
+					verts[#verts+1] = {{x+dotDims,y+dotDims,0}, color}
+					verts[#verts+1] = {{x+dotDims,y-dotDims,0}, color}
+					verts[#verts+1] = {{x-dotDims,y-dotDims,0}, color}
+				end
+			end
+			self:SetVertices(verts)
+			self:SetDrawState{Mode="DrawMode_Quads"}
+			self:SetDrawState{First = 1, Num=#verts}
+		end,
+}
+
 -- Early/Late markers
 o[#o+1] = LoadFont("Common Normal")..{
 	InitCommand=cmd(xy,-plotWidth/2,-plotHeight/2+2;settextf,"Late (+%ims)", maxOffset;zoom,0.35;halign,0;valign,0),
